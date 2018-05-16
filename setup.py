97bdeb4d7ac4af4b0ba605ae9bcf3583046554e9
YanYuHl <A@Thoxvi.com>
2018-05-17 02:44:59 +0800

    abs(version)

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='fc',
-    version='-0.0.3',
+    version='0.0.3',
     description='Enables Python to be functionally chain-styled.',
     long_description=long_description,
     long_description_content_type='text/markdown',

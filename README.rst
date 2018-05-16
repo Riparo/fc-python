f059e509bcb9be9a085455bfb1e23eea5de2cac4
YanYuHl <A@Thoxvi.com>
2018-05-17 03:32:29 +0800

    fix setip.py

--- README.rst
+++
@@ -1,6 +0,0 @@
-# Fuck-Py-Functional
-
-[![Build Status](https://travis-ci.org/Thoxvi/Fuck-Py-Functional.svg?branch=master)](https://travis-ci.org/Thoxvi/Fuck-Py-Functional)
-
-Enables Python to be functionally chain-styled.
-
--- setup.py
+++ setup.py
@@ -1,18 +1,10 @@
 from setuptools import setup, find_packages
-from codecs import open
-from os import path
-
-here = path.abspath(path.dirname(__file__))
-
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
 
 setup(
     name='fc',
-    version='0.0.3',
+    version='0.0.9',
     description='Enables Python to be functionally chain-styled.',
-    long_description=long_description,
-
+    python_requires='>=3.4',
     url='https://github.com/Thoxvi/Fuck-Py-Functional',
     author='Thoxvi',
     author_email='A@Thoxvi.com',
@@ -20,9 +12,6 @@ setup(
     classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: Implementation',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
@@ -30,5 +19,7 @@ setup(
     ],
     keywords='lambda function',
 
-    packages=find_packages(),
+    packages=[
+        "function_chain"
+    ],
 )

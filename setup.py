cca8b467eabd65292235287715cae65ffc446250
YanYuHl <A@Thoxvi.com>
2018-05-17 03:09:22 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -18,12 +18,17 @@ setup(
     author_email='A@Thoxvi.com',
 
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries'
     ],
     keywords='lambda function',
 
-    packages=find_packages(exclude=['function_chain']),
+    packages=find_packages(),
 )
